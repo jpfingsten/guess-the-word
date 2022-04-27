a6daec41c6bb0aa6ae2efd4fc72e4e706d856cc5
jpfingsten <jasmine.pfingsten@gmail.com>
2022-04-27 17:16:22 -0400

    Cleared away unnecessary variables, finished update word section

--- js/script.js
+++ js/script.js
@@ -12,10 +12,11 @@ const guessedLetters = [];
 
 //creating dots for the number of letters in the mystery word
 const wordThisRound = function () {
-    const wordArray = word.split("");
-    for (let letter in wordArray) {
-        wordInProgress.append("●")
+    const placeholderLetters = [];
+    for (const letter of word) {
+        placeholderLetters.push("●");
     }
+    wordInProgress.innerText = placeholderLetters.join("");
 }
 
 wordThisRound(word);
@@ -24,13 +25,13 @@ wordThisRound(word);
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     const playerGuess = textInput.value;
-    textInput.value = "";
     message.innerText = "";
     const validatedGuess = validateInput(playerGuess);
     if (validatedGuess) {
         makeGuess(validatedGuess);
     }
-})
+    textInput.value = "";
+});
 
 //validate player's input
 const validateInput = function (input) {
@@ -39,26 +40,23 @@ const validateInput = function (input) {
         message.innerText = "Please guess a letter.";
     } else if (input.length >= 2) {
         message.innerText = "One letter at a time!";
-        input = "";
     } else if (!input.match(acceptedLetter)) {
         message.innerText = "Letters only, please!";
-        input = "";
-    };
-
-    return input;
+    } else {
+        return input;
+    }
 }
 
 //capture input
-const makeGuess = function (letter) {
-    const thisLetter = letter;
-    const upperCaseLetter = thisLetter.toUpperCase();
-    if (guessedLetters.includes(upperCaseLetter)) {
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
         message.innerText = "You've already guessed that letter. Try again!";
     } else {
-        guessedLetters.push(upperCaseLetter);
+        guessedLetters.push(guess);
         updateGuessedList();
+        updateWordInProgress(guessedLetters);
     }
-    console.log(guessedLetters);
 }
 
 //show the guessed letters
@@ -66,23 +64,25 @@ const updateGuessedList = function () {
     guessedList.innerHTML = "";
     for (let letter of guessedLetters) {
         const li = document.createElement("li");
-        li.innerText = `${letter}`;
+        li.innerText = letter;
         guessedList.append(li);
     }
 }
 
-//update circle symbols with correctly guessed letters, NOT COMPLETE
-const updateWord = function (guessedLetters) {
+//update circle symbols with correctly guessed letters
+const updateWordInProgress = function (guessedLetters) {
     const wordUpper = word.toUpperCase();
     const wordArray = wordUpper.split("");
     const guessedLettersArray = [];
-    for (let letter of wordArray) { //start from here on next attempt
+    console.log(wordArray);
+    for (let letter of wordArray) {
         if (guessedLetters.includes(letter)) {
-            guessedLettersArray.append(`${letter}`)
+            guessedLettersArray.push(letter.toUpperCase());
         } else {
-            guessedLettersArray.append("●")
-        };
-    };
-    const newGuessedList = guessedLettersArray.join();
-    wordInProgress.innerText = newGuessedList;
+            guessedLettersArray.push("●");
+        }
+    }
+    console.log(guessedLettersArray);
+    wordInProgress.innerText = guessedLettersArray.join("");
 }
+updateWordInProgress();
